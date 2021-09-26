5f65366482120b0eab3c3e2b98ebd5375ae92346
Mathew Harris <10959101@uvu.edu>
2021-09-25 20:02:38 -0600

    Loops

---
+++ Beginner Scripting WK 1/Assets/DoWhileLoop.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoWhileLoop : MonoBehaviour
+{
+    
+    void Start()
+    {
+        bool shouldContinue = false;
+
+        do
+        {
+            print("Hello There");
+            // edited from exampleto be more my nerdome
+        }
+        while (shouldContinue == true);
+
+    }
+
+  
+}
---
+++ Beginner Scripting WK 1/Assets/ForLoop.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour
+{
+
+    int numEnemies = 3;
+
+
+    void Start()
+    {
+        for (int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating Enemy Number:" + i);
+
+        }
+    }
+
+}
