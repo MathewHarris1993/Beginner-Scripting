f8cc4814018bc3e4ad7bca469048b7027519b79c
Mathew Harris <10959101@uvu.edu>
2021-09-25 20:18:20 -0600

    Awake and Start

---
+++ Beginner Scripting WK 1/Assets/AwakeandStart.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using UnityEngine;
+
+public class AwakeandStart : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake called.");
+
+    }
+
+    void Start()
+    {
+        Debug.Log("Start called.");
+
+    }
+}
